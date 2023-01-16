3550fe14724b54bf5b4c308cc971ce12d7f2d7fc
Elif Tosun <92747017+elifftosunn@users.noreply.github.com>
2023-01-16 03:06:09 +0300

    Update trainValTestForEveryData.py

--- trainValTestForEveryData.py
+++ trainValTestForEveryData.py
@@ -1,14 +1,16 @@
 import os, re
 from pathlib import Path
 
-IMAGES_LABELS_PATH = "/home/eliftosun/Desktop/droneImages/imagesTotal" # image+label folder(same folder)
-# print(os.listdir(IMAGE_PATH))
-TRAIN_PATH = "/home/eliftosun/Desktop/droneImages/train"
-TEST_PATH = "/home/eliftosun/Desktop/droneImages/test"
-VAL_PATH = "/home/eliftosun/Desktop/droneImages/val"
-# os.mkdir(TRAIN_PATH)
-# os.mkdir(VAL_PATH)
-# os.mkdir(TEST_PATH)
+# train-val-test split for every datas (image-label at same folder)
+
+IMAGES_LABELS_PATH = "../imagesTotal" # image+label folder(same folder)
+# print(os.listdir(IMAGES_LABELS_PATH))
+TRAIN_PATH = "../train"
+TEST_PATH = "../test"
+VAL_PATH = "../val"
+os.mkdir(TRAIN_PATH)
+os.mkdir(VAL_PATH)
+os.mkdir(TEST_PATH)
 def split(TRAIN_OR_VAL_OR_TEST_PATH, cnt):
     regex = "[a-zA-Z0-9_-]+"
     for image in os.listdir(IMAGES_LABELS_PATH):
@@ -29,8 +31,8 @@ def split(TRAIN_OR_VAL_OR_TEST_PATH, cnt):
                 break
         else:
             print("NOT FOUND IMAGE AND LABEL")
-    # print(os.listdir(IMAGES_LABELS_PATH))
-# split(TRAIN_PATH, 890)
+split(TRAIN_PATH, 890)
+
 # print(1272*0.7) # 890 train
 # print(1272*0.3) # 191 val, 191 test
-# print(len(os.listdir(IMAGES_LABELS_PATH))/2) # => 1272
+# print(len(os.listdir(IMAGES_LABELS_PATH))/2) # => 1272 images, 1272 labels
